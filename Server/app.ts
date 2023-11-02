1a3204c3b314ff46cfd18a4b28771c451a956788
ra334 <mikegrin11@proton.me>
2023-11-02 16:28:41 +0200

    http2

--- Server/app.ts
+++ Server/app.ts
@@ -1,17 +1,26 @@
 import express from 'express';
+import http2 from 'http2';
+import http2Express from 'http2-express-bridge'
+import fs from 'fs';
 import { Request, Response} from 'express';
 import helmet from 'helmet'
 import cors from 'cors'
-const app = express()
-
+import router from './router/router'
+const app = http2Express(express)
 
 app.use(cors())
 app.use(helmet())
+app.use('/api', router)
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('hello')
+    res.send('hello')  
 })
 
-app.listen(3300, () => {
-    console.log('App started')
+const server = http2.createSecureServer({
+    key: fs.readFileSync(`${__dirname}/cert/server.key`),
+    cert: fs.readFileSync(`${__dirname}/cert/server.cert`)
+}, app)
+
+server.listen(3300, () => {
+    console.log('server is running on port 3300')
 })
