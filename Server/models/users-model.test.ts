1d07b78d7ac2f4e7d4532fe5158596594b108e45
ra334 <mikegrin11@proton.me>
2023-11-13 18:49:36 +0200

    update user model tests

--- Server/models/users-model.test.ts
+++ Server/models/users-model.test.ts
@@ -35,7 +35,7 @@ describe("User model", () => {
     const mockPrismaCreate = (value: User) =>
         prismaAdapter.users.create.mockResolvedValue(value);
 
-    const mockPrismaGet = (value: User) =>
+    const mockPrismaGet = (value: User | null) =>
         prismaAdapter.users.findFirst.mockResolvedValue(value);
 
     const mockPrismaUpdate = (value: User) =>
@@ -62,11 +62,94 @@ describe("User model", () => {
     test("should get a user by id", async () => {
         mockPrismaGet(user)
 
-        const { id, login } = await userModel.getUser(userID);
+        const result = await userModel.getUser(userID);
         
-        expect(id).toBe(userID);
-        expect(login).toBe(user.login);
+        if (result) {
+            const { id, login } = result
+
+            expect(id).toBe(userID);
+            expect(login).toBe(user.login);
+        }
     })
+
+    test("should get user by nickname", async () => {
+        mockPrismaGet(user)
+
+        const result = await userModel.getUser(user.nickname);
+
+        if (result) {
+            const { id, login } = result;
+
+            expect(id).toBe(userID)
+            expect(login).toBe(user.login)
+        }
+    })
+
+    test("should update a password", async () => {
+        const newPassword = 'new_password';
+        mockPrismaUpdate(user)
+
+        const result = await userModel.updateUserPassword(userID, newPassword)
+
+        if (result) {
+            const { id, password } = result
+
+            expect(id).toBe(userID)
+            expect(password).toBe(newPassword)
+        }
+    })
+
+    test("should update user role", async () => {
+        const newRole = "admin";
+        mockPrismaUpdate(user);
+    
+        const result = await userModel.updateUserRole(userID, newRole);
+    
+        if (result) {
+            const { id, role } = result;
+            expect(id).toBe(userID);
+            expect(role).toBe(newRole);
+        }
+    });
+    
+    test("should update user last login", async () => {
+        const newLastLogin = new Date();
+        mockPrismaUpdate(user);
+    
+        const result = await userModel.updateUserLastLogin(userID, newLastLogin);
+    
+        if (result) {
+            const { id, last_login } = result;
+            expect(id).toBe(userID);
+            expect(last_login).toEqual(newLastLogin);
+        }
+    });
+    
+    test("should update user account status", async () => {
+        const newAccountStatus = "inactive";
+        mockPrismaUpdate(user);
+    
+        const result = await userModel.updateUserAccountStatus(userID, newAccountStatus);
+    
+        if (result) {
+            const { id, account_status } = result;
+            expect(id).toBe(userID);
+            expect(account_status).toBe(newAccountStatus);
+        }
+    });
+    
+    test("should update user profile picture", async () => {
+        const newProfilePicture = Buffer.from("new_picture_data");
+        mockPrismaUpdate(user);
+    
+        const result = await userModel.updateUserProfilePicture(userID, newProfilePicture);
+    
+        if (result) {
+            const { id, profile_picture } = result;
+            expect(id).toBe(userID);
+            expect(profile_picture).toEqual(newProfilePicture);
+        }
+    });
 
     test("should delete a user", async () => {
         mockPrismaDelete(user);
