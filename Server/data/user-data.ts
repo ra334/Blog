62063027b081a8bdcb900ee6aa6c183584a48aed
ra334 <mikegrin11@proton.me>
2023-11-19 14:59:21 +0200

    update: remove few unused propertys

--- Server/data/user-data.ts
+++ Server/data/user-data.ts
@@ -1,18 +1,13 @@
-import { User } from "../types/test-types";
 import {v4 as uuidv4} from 'uuid'
 import * as fs from 'fs'
 
 const userID = uuidv4()
 
-const user: User = {
+const user = {
     id: userID,
     login: "John",
     password: "password",
     nickname: "John_" + userID, 
-    role: "user",
-    last_login: new Date(),
-    account_created: new Date(),
-    account_status: "active",
     profile_picture: fs.readFileSync("./assets/logo.png"),
 };
 
