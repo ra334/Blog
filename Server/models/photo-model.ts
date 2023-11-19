146f6e6d79907b7fca424a4579edcc520b469bdc
ra334 <mikegrin11@proton.me>
2023-11-19 14:56:15 +0200

    update: add argument id to method createPhoto(IMG and URL)

--- Server/models/photo-model.ts
+++ Server/models/photo-model.ts
@@ -2,16 +2,41 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 class PhotoModel {
-    async createPhoto(postID: string, image: Buffer) {
+    async createPhotoIMG(
+        photoID: string, 
+        postID: string, 
+        image: Buffer
+        ) {
         try {
             await prisma.$connect()
             const photo = await prisma.photos.create({
                 data: {
+                    id: photoID,
                     post_id: postID,
                     image 
                 }
             })
             
+            return photo
+        } catch(e) {
+            console.error(e)
+            throw(e)
+        } finally {
+            await prisma.$disconnect()
+        }
+    }
+
+    async createPhotoURL(photoID: string, postID: string, url: string) {
+        try {
+            await prisma.$connect()
+            const photo = await prisma.photos.create({
+                data: {
+                    id: photoID,
+                    post_id: postID,
+                    url
+                }
+            })
+
             return photo
         } catch(e) {
             console.error(e)
@@ -44,6 +69,38 @@ class PhotoModel {
             const photo = await prisma.photos.update({
                 where: {id: photoID},
                 data: {image}
+            })
+
+            return photo
+        } catch(e) {
+            console.error(e)
+            throw(e)
+        } finally {
+            await prisma.$disconnect()
+        }
+    }
+
+    async getPhotoByID(photoID: string) {
+        try {
+            await prisma.$connect()
+            const photo = await prisma.photos.findFirst({
+                where: {id: photoID}
+            })
+            
+            return photo
+        } catch(e) {
+            console.error(e)
+            throw(e)
+        } finally {
+            await prisma.$disconnect()
+        }
+    }
+
+    async getPhotos(postID: string) {
+        try {
+            await prisma.$connect()
+            const photo = await prisma.photos.findMany({
+                where: {post_id: postID}
             })
 
             return photo
