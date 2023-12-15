773daa3ab685b47c0cf7bf329c9844873ce7f94a
ra334 <savcukmihajlo@gmail.com>
2023-12-15 11:49:54 +0200

    test: add test for comparePassword func

--- Server/tools/hash.test.ts
+++ Server/tools/hash.test.ts
@@ -1,23 +1,33 @@
-import { describe, it, expect } from 'vitest';
-import * as bcrypt from 'bcrypt';
-import hashPassword from './hash';
+import { describe, it, expect } from "vitest";
+import * as bcrypt from "bcrypt";
+import { hashPassword, comparePassword } from "./hash";
 
-describe('hashPassword function', () => {
-  it('should hash the password correctly', () => {
-    const password = 'testPassword'
-    const hashedPassword = hashPassword(password)
+describe("hashPassword function", () => {
+    const password = 'password'
+    let hash = ''
 
-    expect(hashedPassword).not.equal(hashPassword, password)
-    expect(bcrypt.compare(password, hashedPassword)).toBeTruthy()
-  });
+    it("should hash the password correctly", () => {
+        const hashedPassword = hashPassword(password);
 
-  it('should generate different hashes for different passwords', () => {
-    const password1 = 'password1';
-    const password2 = 'password2';
+        hash = hashedPassword
 
-    const hashedPassword1 = hashPassword(password1);
-    const hashedPassword2 = hashPassword(password2);
+        expect(hashedPassword).not.equal(hashPassword, password);
+        expect(bcrypt.compare(password, hashedPassword)).toBeTruthy();
+    });
 
-    expect(hashedPassword1).not.equal(hashedPassword2)
-  });
+    it("should generate different hashes for different passwords", () => {
+        const password1 = "password1";
+        const password2 = "password2";
+
+        const hashedPassword1 = hashPassword(password1);
+        const hashedPassword2 = hashPassword(password2);
+
+        expect(hashedPassword1).not.equal(hashedPassword2);
+    });
+
+    it("should compare password", () => {
+        const isValid = comparePassword(password, hash)
+
+        expect(isValid).toBeTruthy()
+    });
 });
