04a6fe1a6f063b9df8d429fc05227355bcbf6dad
ra334 <savcukmihajlo@gmail.com>
2023-12-17 14:44:47 +0200

    test: created tests

--- Server/services/users-service.test.ts
+++ Server/services/users-service.test.ts
@@ -1,4 +1,5 @@
-import { test, vi, describe, expect, afterAll, beforeEach } from 'vitest';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { test, vi, describe, expect, afterAll } from 'vitest';
 import userService from './users-service';
 import userModel from '../models/users-model'
 
@@ -10,7 +11,10 @@ describe('User service', () => {
     const validNickname = 'darkk_mike'
     const validPassword = 'mike1234'
 
-    const incorectLogin = '1234567890123456789012345678901234567890123456789011'
+    const invalidLogin = 'mike33'
+    const invalidPassword = 'mike12345'
+
+    const incorrectLogin = '1234567890123456789012345678901234567890123456789011'
     const incorrectPassword = '123456789012345678901234567890123456789012345678901234567890123456789012345678901'
     const incorrectNickname = '123456789012345678901'
 
@@ -18,6 +22,8 @@ describe('User service', () => {
         const user = await userModel.getUserByNickname(validNickname)
         if (user) {
             await userModel.deleteUser(user.id)
+        } else {
+            throw new Error('AfterAll error')
         }
     })
 
@@ -29,9 +35,37 @@ describe('User service', () => {
             expect(tokens.refreshToken).toBeTruthy()
         })
 
-        test('register with invalid data', async () => {
+        test('register with invalid login', async () => {
+            try {
+                await userService.registration(incorrectLogin, validNickname, validPassword);
+            } catch (error: any) {
+                expect(error.message).toBe('Login is too long');
+            }
+        });
 
-        })
+        test('register with invalid nickname', async () => {
+            try {
+                await userService.registration(validLogin, incorrectNickname, validPassword);
+            } catch (error: any) {
+                expect(error.message).toBe('Nickname is too long');
+            }
+        });
+
+        test('register with invalid password', async () => {
+            try {
+                await userService.registration(validLogin, validNickname, incorrectPassword);
+            } catch (error: any) {
+                expect(error.message).toBe('Password is too long');
+            }
+        });
+
+        test('register with existing user', async () => {
+            try {
+                await userService.registration(validLogin, validNickname, validPassword);
+            } catch (error: any) {
+                expect(error.message).toBe('User is already exist');
+            }
+        });
     })
 
     describe('login', async () => {
@@ -40,6 +74,22 @@ describe('User service', () => {
             
             expect(tokens.accessToken).toBeTruthy()
             expect(tokens.refreshToken).toBeTruthy()
+        })
+
+        test('login with invalid data', async () => {
+            try {
+                await userService.login(invalidLogin, validNickname, validPassword)
+            } catch (error: any) {
+                expect(error.message).toBe('User not found!')
+            }
+        })
+
+        test('login with incorrect password', async () => {
+            try {
+                await userService.login(validLogin, validNickname, invalidPassword)
+            } catch (error: any) {
+                expect(error.message).toBe('Password invalid!')
+            }
         })
     })
 })
