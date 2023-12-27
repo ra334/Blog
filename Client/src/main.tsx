df6beccf9c54271a317c186d6f7b7309d581a3dd
ra334 <savcukmihajlo@gmail.com>
2023-12-27 20:05:38 +0200

    chore: update

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import "normalize.css";
+import router from "./router";
+import store from "./store/store";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    </React.StrictMode>
+);
