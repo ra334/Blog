4d172a1a553f5b421596e23a6700cbd7c1a4aa4f
ra334 <savcukmihajlo@gmail.com>
2023-12-27 20:09:31 +0200

    chore: update

--- Server/app.ts
+++ Server/app.ts
@@ -1,27 +1,27 @@
 import * as express from 'express';
-import * as http2 from 'http2';
-import http2Express = require('http2-express-bridge');
-import * as fs from 'fs';
 import { Request, Response} from 'express';
 import helmet from 'helmet'
 import cors = require('cors');
+import bodyParser = require('body-parser');
+import cookieParser = require('cookie-parser');
 import router from './router/router'
-const app = http2Express(express)
+import ErrorHandlingMiddleware from './middleware/ErrorHandlingMiddleware';
+const app = express()
 
 app.use(cors())
 app.use(helmet())
+app.use(bodyParser())
+app.use(cookieParser())
 app.use('/api', router)
+app.use(ErrorHandlingMiddleware)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('hello')  
-    res.status(200).end()
+    res.status(200)
 })
 
-const server = http2.createSecureServer({
-    key: fs.readFileSync('./cert/server.key'),
-    cert: fs.readFileSync('./cert/server.cert'),
-}, app)
-
-server.listen(443, () => {
-    console.log('server is running https://localhost')
+app.listen(8080, () => {
+    console.log('listening http://localhost:8080')
 })
+
+
