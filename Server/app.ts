5d1f5def1ef4ddc4bd63aac62276bf6455a706d1
ra334 <savcukmihajlo@gmail.com>
2024-01-01 10:47:16 +0200

    chore: add api and content routes

--- Server/app.ts
+++ Server/app.ts
@@ -1,27 +1,27 @@
 import * as express from 'express';
-import { Request, Response} from 'express';
 import helmet from 'helmet'
 import cors = require('cors');
 import bodyParser = require('body-parser');
 import cookieParser = require('cookie-parser');
-import router from './router/router'
+import apiRouter from './router/api/api-router'
+import contentRouter from './router/content/content-router'
 import ErrorHandlingMiddleware from './middleware/ErrorHandlingMiddleware';
+import path = require('path')
 const app = express()
 
-app.use(cors())
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 app.use(helmet())
 app.use(bodyParser())
 app.use(cookieParser())
-app.use('/api', router)
+app.use('/', contentRouter)
+app.use('/api', apiRouter)
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(ErrorHandlingMiddleware)
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('hello')  
-    res.status(200)
-})
 
 app.listen(8080, () => {
     console.log('listening http://localhost:8080')
 })
-
-
