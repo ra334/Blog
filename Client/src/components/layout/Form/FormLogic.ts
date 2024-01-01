65da229467c88bb94d4f55255b27b4d627de1bf1
ra334 <savcukmihajlo@gmail.com>
2024-01-01 10:35:06 +0200

    feat: create subbmiting function

---
+++ Client/src/components/layout/Form/FormLogic.ts
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+type AuthDataType = {
+    login: string;
+    nickname: string;
+    password: string;
+}
+
+class FormLogic {
+
+    async subbmiting(authData: AuthDataType){
+        const response = await axios({
+            method: 'post',
+            url: 'http://localhost:8080/api/registration',
+            data: {
+                login: authData.login,
+                nickname: authData.nickname,
+                password: authData.password
+            },
+            withCredentials: true,
+        });
+
+        if (response.status === 200) {
+            window.history.pushState({}, 'undefined', '/')
+            window.location.reload()
+        }
+    }
+}
+
+export default new FormLogic()
