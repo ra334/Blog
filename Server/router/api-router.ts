47e20aeaec68bafba0ac0973e51d6031c4993e08
ra334 <savcukmihajlo@gmail.com>
2024-01-07 21:01:39 +0200

    chore: rename file router

---
+++ Server/router/api-router.ts
@@ -0,0 +1,40 @@
+import express = require('express')
+const router = express.Router()
+import { Request, Response, NextFunction } from "express"
+import userController from "../controller/users-controller"
+import articleController from "../controller/posts-controller"
+
+
+router.post('/login', (req: Request, res: Response, next: NextFunction) => {
+    userController.login(req, res, next)
+})
+
+router.post('/registration', (req: Request, res: Response, next: NextFunction) => {
+    userController.registration(req, res, next)
+})
+
+router.post('/logout', (req: Request, res: Response) => {
+    userController.logout(req, res)
+})
+
+router.get('/articles', (req: Request, res: Response) => {
+    articleController.getArticles(req, res)
+})
+
+router.get('/articles:id', (req: Request, res: Response) => {
+    articleController.getArticles(req, res)
+})
+
+router.post('/create', (req: Request, res: Response) => {
+    articleController.createArticle(req, res)
+})
+
+router.delete('/articles:id', (req: Request, res: Response) => {
+    articleController.deleteArticle(req, res)
+})
+
+router.patch('/articles:id', () => {
+    
+})
+
+export default router
