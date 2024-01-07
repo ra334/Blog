395ba63f1ab76359f372e083ad9221fc78a3a1da
ra334 <savcukmihajlo@gmail.com>
2024-01-07 21:15:54 +0200

    feat: create header auth behavior

--- Client/src/components/layout/Header/Header.tsx
+++ Client/src/components/layout/Header/Header.tsx
@@ -1,17 +1,38 @@
 import { Link } from "react-router-dom"
+import './HeaderStyle.css'
 
 type HeaderPropsType = {
     isSignIn: boolean;
+    accountName: string;
 }
 
 function Header(props: HeaderPropsType) {
+
+    function signIn(isSignIn: boolean) {
+        if (isSignIn) {
+            return (
+                <Link to="/account">{props.accountName}</Link>
+            )
+        }
+
+        return (
+            <div className="signIn">
+                <Link to="/login">SignIn</Link>
+                <div className="signIn__slash">/</div>
+                <Link to="/registration">Registration</Link>
+            </div>
+        )
+    }
+
     return (
         <header>
-            <nav className="nav">
-                <Link to="/login">Write</Link>
-                <Link to="/registration">List articles</Link>
-                <Link to="#">Search articles</Link>
-            </nav>
+            <div className="container">
+                <nav className="nav">
+                    <Link to="/write">Write</Link>
+                    <Link to="/articles">List articles</Link>
+                    {signIn(props.isSignIn)}
+                </nav>
+            </div>
         </header>
     )
 }
