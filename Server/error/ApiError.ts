ffd296adee2c0bc69f172e16aa3ec100247ac527
ra334 <savcukmihajlo@gmail.com>
2024-01-07 21:10:06 +0200

    chore: move function

--- Server/error/ApiError.ts
+++ Server/error/ApiError.ts
@@ -7,16 +7,16 @@ class ApiError extends Error {
         this.message = message
     }
 
+    static forbidden(message: string) {
+        return new ApiError(403, message)
+    }
+
     static badRequest(message: string) {
-        return new ApiError(404, message)
+        return new ApiError(400, message)
     }
 
     static internal(message: string) {
         return new ApiError(500, message)
-    }
-
-    static forbidden(message: string) {
-        return new ApiError(403, message)
     }
 }
 
