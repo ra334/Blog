597c672a546b136c832c72627c809330ce10d499
ra334 <savcukmihajlo@gmail.com>
2024-01-08 13:48:55 +0200

    chore: change border style and rename class

--- Client/src/components/layout/Form/Form.tsx
+++ Client/src/components/layout/Form/Form.tsx
@@ -17,45 +17,44 @@ function Form(props: FormProps) {
     const authData = useSelector((state: any) => state.auth)
     const [error, setError] = useState<string | null>(null);
 
+
     async function handleSubmit() {
         try {
-            await subbmiting(authData)
+            await subbmiting(authData, )
         } catch(err: any) {
             setError(err.message)
         }
     }
 
     return (
-        <div className="container">
-            <div className="wrapper">
-                <h1 className="title">{props.h1Text}</h1>
-                {error && <ErrorComponent message={error} />}
-                <div className="input__wrapper">
-                    <input
-                        type="text"
-                        placeholder="Login"
-                        value={authData.login}
-                        onChange={(e) => dispatch(addLogin(e.target.value))}
-                    />
-                </div>
-                <div className="input__wrapper">
-                    <input
-                        type="text"
-                        placeholder="Nickname"
-                        value={authData.nickname}
-                        onChange={(e) => dispatch(addNickname(e.target.value))}
-                    />
-                </div>
-                <div className="input__wrapper">
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={authData.password}
-                        onChange={(e) => dispatch(addPassword(e.target.value))}
-                    />
-                </div>
-                <Button text={props.h1Text} onClick={handleSubmit}/>
+        <div className="form__wrapper">
+            <h1 className="title">{props.h1Text}</h1>
+            {error && <ErrorComponent message={error} />}
+            <div className="input__wrapper">
+                <input
+                    type="text"
+                    placeholder="Login"
+                    value={authData.login}
+                    onChange={(e) => dispatch(addLogin(e.target.value))}
+                />
             </div>
+            <div className="input__wrapper">
+                <input
+                    type="text"
+                    placeholder="Nickname"
+                    value={authData.nickname}
+                    onChange={(e) => dispatch(addNickname(e.target.value))}
+                />
+            </div>
+            <div className="input__wrapper">
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={authData.password}
+                    onChange={(e) => dispatch(addPassword(e.target.value))}
+                />
+            </div>
+            <Button text={props.h1Text} onClick={handleSubmit}/>
         </div>
     );
 }
