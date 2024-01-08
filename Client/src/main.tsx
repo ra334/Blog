63ffe3c653aa9998c5245892bbf8a1dd40f69186
ra334 <savcukmihajlo@gmail.com>
2024-01-08 13:52:51 +0200

    feat: work with cookies

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-import "./index.css";
 import "normalize.css";
+import "./index.css";
+import { CookiesProvider } from 'react-cookie';
 import router from "./router";
 import store from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
+        <CookiesProvider defaultSetOptions={{path: '/'}}>
+            <Provider store={store}>
+                <RouterProvider router={router} />
+            </Provider>
+        </CookiesProvider>
     </React.StrictMode>
 );
