2c24036536bb64f1dd6a81f96584293280dec3f7
ra334 <savcukmihajlo@gmail.com>
2024-01-15 14:07:25 +0200

    feat: type animation

--- Client/src/pages/Main/MainPage.tsx
+++ Client/src/pages/Main/MainPage.tsx
@@ -1,16 +1,22 @@
 import Header from '../../components/layout/Header/Header'
 import './MainPageStyle.css'
+import { TypeAnimation } from 'react-type-animation';
 
 function Main() {
+
     return (
-        <>
-            <div className="container">
-                <Header/>
-                <main className='main'>
-                    <h1 className='main__title'>Create your blog for free</h1>
-                </main>
-            </div>
-        </>
+        <div className="container">
+            <Header/>
+            <main className='main'>
+                <TypeAnimation
+                    sequence={['Write blog for free', 3000, 'Create your own blog', 3000]}
+                    wrapper='span'
+                    speed={30}
+                    style={{marginTop: '10%',fontWeight: 700, fontSize: '50px'}}
+                    repeat={Infinity}
+                />
+            </main>
+        </div>
     )
 }
 
