2ecb48f3983ba66fbf6fdb57eb3d3395e24b29f8
ra334 <savcukmihajlo@gmail.com>
2024-01-15 10:24:25 +0200

    init commit

---
+++ Client/src/pages/Syntax/SyntaxPage.tsx
@@ -0,0 +1,12 @@
+import Header from "../../components/layout/Header/Header"
+
+function SyntaxPage() {
+    return (
+        <div className="container">
+            <Header />
+            <h1>SyntaxPage</h1>
+        </div>
+    )
+}
+
+export default SyntaxPage
