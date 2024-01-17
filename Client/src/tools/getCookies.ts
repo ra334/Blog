723c86a4e4883d3e84c8e017e87214f7aafade7a
ra334 <savcukmihajlo@gmail.com>
2024-01-17 11:43:28 +0200

    feat: create tool getCookies

---
+++ Client/src/tools/getCookies.ts
@@ -0,0 +1,27 @@
+interface TokensInterface {
+    [key: string]: string
+}
+
+export function getCookieValue(key: string): string | null {
+    const cookiesString = document.cookie
+    const regex = new RegExp(`${key}=([^;]+)`);
+    const match = cookiesString.match(regex);
+    return match ? match[1] : null;
+}
+
+export function getCookies(): TokensInterface | null {
+    const cookies = document.cookie.split(';')
+
+    if (cookies.length === 0) return null
+
+    const cookieObj: TokensInterface = {}
+
+    cookies.forEach((cookie) => {
+        const parts = cookie.split('=')
+        const name = parts[0]
+        const value = parts[1]
+        cookieObj[name] = value
+    })
+
+    return cookieObj
+}
