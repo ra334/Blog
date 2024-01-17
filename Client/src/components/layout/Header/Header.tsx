f4b925f45001fbbe0a49886e35ef0e2d9d762e3a
ra334 <savcukmihajlo@gmail.com>
2024-01-17 12:07:28 +0200

    refactor

--- Client/src/components/layout/Header/Header.tsx
+++ Client/src/components/layout/Header/Header.tsx
@@ -1,39 +1,56 @@
-import { Link } from "react-router-dom"
-import './HeaderStyle.css'
+import { Link } from "react-router-dom";
+import './HeaderStyle.css';
 import { jwtDecode } from "jwt-decode";
-import { PayloadType, TokensType } from '../../../types/tokens-type'
+import { PayloadType, TokensType } from '../../../types/tokens-type';
 import verifyAccessToken from "../../../tools/verifyAccessToken";
 import { getCookies } from "../../../tools/getCookies";
 
 function Header() {
-    const cookies = getCookies()
-    const isSignIn = Object.keys(cookies).length > 0 ? true : false
+    const cookies = getCookies();
+
+    if (!cookies) {
+        return (
+            <header>
+                <nav className="nav">
+                    <Link className="header__link" to="/">Main page</Link>
+                    <Link className="header__link" to="/write">Write</Link>
+                    <Link className="header__link" to="/articles">List articles</Link>
+                    <Link className="header__link" to="/syntax">Article syntax</Link>
+                    <div className="signIn">
+                        <Link className="header__link" to="/login">SignIn</Link>
+                        <div className="signIn__slash">/</div>
+                        <Link className="header__link" to="/registration">Registration</Link>
+                    </div>
+                </nav>
+            </header>
+        );
+    }
+
+    const isSignIn = Object.keys(cookies).length > 0;
 
     if (isSignIn) {
         const tokens: TokensType = {
             accessToken: cookies.accessToken,
             refreshToken: cookies.refreshToken
-        }
+        };
 
-        verifyAccessToken.verifyTokens(tokens)
-    }
+        verifyAccessToken.verifyTokens(tokens);
 
-    function signIn(isSignIn: boolean) {
-        if (isSignIn) {
+        if (cookies.accessToken) {
             const payload: PayloadType = jwtDecode(cookies.accessToken);
-            const accountName = payload.nickname
+            const accountName = payload.nickname;
             return (
-                <Link className="header__link" to="/account">{accountName}</Link>
-            )
+                <header>
+                    <nav className="nav">
+                        <Link className="header__link" to="/" >Main page</Link>
+                        <Link className="header__link" to="/write">Write</Link>
+                        <Link className="header__link" to="/articles">List articles</Link>
+                        <Link className="header__link" to="/syntax">Article syntax</Link>
+                        <Link className="header__link" to="/account">{accountName}</Link>
+                    </nav>
+                </header>
+            );
         }
-
-        return (
-            <div className="signIn">
-                <Link className="header__link" to="/login">SignIn</Link>
-                <div className="signIn__slash">/</div>
-                <Link className="header__link" to="/registration">Registration</Link>
-            </div>
-        )
     }
 
     return (
@@ -43,10 +60,14 @@ function Header() {
                 <Link className="header__link" to="/write">Write</Link>
                 <Link className="header__link" to="/articles">List articles</Link>
                 <Link className="header__link" to="/syntax">Article syntax</Link>
-                {signIn(isSignIn)}
+                <div className="signIn">
+                    <Link className="header__link" to="/login">SignIn</Link>
+                    <div className="signIn__slash">/</div>
+                    <Link className="header__link" to="/registration">Registration</Link>
+                </div>
             </nav>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
