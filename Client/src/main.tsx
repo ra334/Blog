d59ce1e31c341e1dfe20eb77580ce52fd573b467
ra334 <savcukmihajlo@gmail.com>
2024-01-17 11:05:34 +0200

    remove: react-cookie

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -4,16 +4,13 @@ import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import "normalize.css";
 import "./index.css";
-import { CookiesProvider } from 'react-cookie';
 import router from "./router";
 import store from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <CookiesProvider defaultSetOptions={{path: '/'}}>
-            <Provider store={store}>
-                <RouterProvider router={router} />
-            </Provider>
-        </CookiesProvider>
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
     </React.StrictMode>
 );
