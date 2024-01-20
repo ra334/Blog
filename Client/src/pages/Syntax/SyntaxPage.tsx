642a76e8d2d0e2dc99b3fd8c0a59f2ec110a7120
ra334 <savcukmihajlo@gmail.com>
2024-01-20 13:39:22 +0200

    init page

--- Client/src/pages/Syntax/SyntaxPage.tsx
+++ Client/src/pages/Syntax/SyntaxPage.tsx
@@ -1,6 +1,11 @@
 import Header from "../../components/layout/Header/Header"
+import { getCookieValue } from "../../tools/getCookies";
+import PageAccessDenied from "../AccessDenied/PageAccessDenied";
 
 function SyntaxPage() {
+    const accessToken = getCookieValue('accessToken');
+    if (!accessToken) return <PageAccessDenied />
+
     return (
         <div className="container">
             <Header />
