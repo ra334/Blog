440ed56ef12eb1051b6b2253fe2e0519be3ab131
ra334 <savcukmihajlo@gmail.com>
2024-01-20 13:40:54 +0200

    init page

--- Client/src/pages/ListArticles.tsx/ListArticles.tsx
+++ Client/src/pages/ListArticles.tsx/ListArticles.tsx
@@ -1,6 +1,17 @@
+import './ListArticles.css'
+import Header from '../../components/layout/Header/Header'
+import { getCookieValue } from '../../tools/getCookies';
+import PageAccessDenied from '../AccessDenied/PageAccessDenied';
+
 function ListArticles() {
+    const accessToken = getCookieValue('accessToken');
+    if (!accessToken) return <PageAccessDenied />
+
     return (
-        <h1>ListArticles</h1>
+        <div className="container">
+            <Header />
+            <h1>Last articles</h1>
+        </div>
     )
 }
 
