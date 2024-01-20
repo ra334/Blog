73b800e5f4316677676b4b98aca0f9f8c99ea802
ra334 <savcukmihajlo@gmail.com>
2024-01-20 13:32:55 +0200

    eslint formating

--- Server/app.ts
+++ Server/app.ts
@@ -1,30 +1,31 @@
-import * as express from 'express';
-import { Request, Response } from 'express';
+import * as express from 'express'
+import { Request, Response } from 'express'
 import helmet from 'helmet'
-import cors = require('cors');
-import bodyParser = require('body-parser');
-import cookieParser = require('cookie-parser');
+import cors = require('cors')
+import bodyParser = require('body-parser')
+import cookieParser = require('cookie-parser')
 import apiRouter from './router/api-router'
-import ErrorHandlingMiddleware from './middleware/ErrorHandlingMiddleware';
+import ErrorHandlingMiddleware from './middleware/ErrorHandlingMiddleware'
 import path = require('path')
 const app = express()
 
-
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+app.use(
+	cors({
+		origin: 'http://localhost:5173',
+		credentials: true,
+	}),
+)
 app.use(helmet())
 app.use(bodyParser())
 app.use(cookieParser())
 app.use('/api', apiRouter)
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')))
 app.use(ErrorHandlingMiddleware)
 
 app.get('*', (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 app.listen(8080, () => {
-    console.log('listening http://localhost:8080')
+	console.log('listening http://localhost:8080')
 })
