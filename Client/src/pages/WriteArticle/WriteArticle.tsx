ccb08f1ce2497a3be30d9a880306fcf82c043dca
ra334 <savcukmihajlo@gmail.com>
2024-01-20 13:36:41 +0200

    create: page write article

--- Client/src/pages/WriteArticle/WriteArticle.tsx
+++ Client/src/pages/WriteArticle/WriteArticle.tsx
@@ -1,7 +1,50 @@
+import { useState } from 'react';
+import './WriteArticle.css';
+import Button from '../../components/UI/Button/Button';
+import Header from '../../components/layout/Header/Header';
+import writeArticleLogic from './WriteArticle-logic';
+import { getCookieValue } from '../../tools/getCookies';
+import PageAccessDenied from '../AccessDenied/PageAccessDenied';
+
+
 function WriteArticle() {
+    const [text, setText] = useState("")
+
+    const accessToken = getCookieValue('accessToken');
+    if (!accessToken) return <PageAccessDenied />
+
+    const handlePreviewClick = () => {
+        console.log('Preview clicked:', 'text');
+    };
+
+    const handleSaveClick = () => {
+        const firstLine = text.split('\n')[0]
+
+        writeArticleLogic.saveArticle(accessToken, firstLine, text)
+    };
+
     return (
-        <h1>WriteArticle</h1>
-    )
+        <div className="container">
+            <Header />
+            <div className="article">
+                <div className="article__title">
+                    <h1>Write new article</h1>
+                </div>
+                <div className="article__content-wrapper">
+                    <textarea
+                        className='article__textarea'
+                        id="textarea"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                    ></textarea>
+                </div>
+                <div className="article__button">
+                    <Button onClick={handlePreviewClick} text="Preview" />
+                    <Button onClick={handleSaveClick} text='Save and publish' />
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default WriteArticle
+export default WriteArticle;
