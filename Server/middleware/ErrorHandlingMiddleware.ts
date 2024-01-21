268ee50054de475e17e27514055d648dc0631ce5
ra334 <savcukmihajlo@gmail.com>
2024-01-21 12:09:26 +0200

    refactor

--- Server/middleware/ErrorHandlingMiddleware.ts
+++ Server/middleware/ErrorHandlingMiddleware.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction} from "express";
 import ApiError from "../error/ApiError";
 
-export default function (err: Error, req: Request, res: Response, next: NextFunction) {
+
+export default function (err: any, req: Request, res: Response, next: NextFunction) {
     if (err instanceof ApiError) {
         return res.status(err.status).json({ message: err.message });
     }
