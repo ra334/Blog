c77628423288e2715b1200367ecfd7cf4fe74349
ra334 <savcukmihajlo@gmail.com>
2024-01-23 10:02:59 +0200

    remove redux inport

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { Provider } from 'react-redux'
 import 'normalize.css'
 import './index.css'
 import router from './router'
-import store from './store/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<RouterProvider router={router} />
-		</Provider>
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 )
