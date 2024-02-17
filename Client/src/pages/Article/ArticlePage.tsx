2eb8fd314fc81d5839e2fc6d1784fb8ac4ee1b05
ra334 <savcukmihajlo@gmail.com>
2024-02-17 12:34:48 +0200

    artilce page change styles

--- Client/src/pages/Article/ArticlePage.tsx
+++ Client/src/pages/Article/ArticlePage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import Header from "../../components/layout/Header/Header"
 import { useParams } from "react-router-dom";
 import getArticle from "./ArticleLogic";
+import './ArticlePage.css'
 
 interface ResponseData {
     created_at: string;
@@ -9,6 +10,16 @@ interface ResponseData {
     text: string;
     title: string;
     user_id: string;
+}
+
+function getDate(date: string): string {
+    const dateTime = new Date(date)
+
+    const year = dateTime.getFullYear()
+    const month = dateTime.getMonth() + 1
+    const day = dateTime.getDate()
+
+    return `${year}-${month < 10 ? '0' : ''}${month}-${day < 10 ? '0' : ''}${day}`;
 }
 
 function ArticlePage() {
@@ -38,9 +49,11 @@ function ArticlePage() {
     return (
         <div className="container">
             <Header />
-            <h1>{title}</h1>
-            <div className="date">{date}</div>
-            <p>{text}</p>
+            <h1 className="article__title">{title}</h1>
+            <div className="article__author">
+                <div className="article__date">{getDate(date)}</div>
+            </div>
+            <p className="article__text">{text}</p>
         </div>
     )
 }
